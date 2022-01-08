22bc6aa099e947d3bfdc03a6e7909c9df057c694
TurkiHaqawi <Turki.Haqawii@gmail.com>
2022-01-08 20:16:07 +0300

    some changes

--- clint/src/requestMethods.js
+++ clint/src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = "http://localhost:8080/api/"
+const BASE_URL = "/api/"
 const TOKEN = "fsddf"
 
 export const publicRequest = axios.create({
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express")
 const app = express()
 const mongoose = require("mongoose")
 const cors = require('cors')
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 5000
 const path = require("path")
 app.use(express.json())
 app.use(cors())
